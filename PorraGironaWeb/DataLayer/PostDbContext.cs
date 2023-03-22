d3a999576e66b9cb2344e6f6e69d907a8477159e
SOC <SOC@IED310-PC10>
2023-03-22 19:44:56 +0100

    add

--- PorraGironaWeb/DataLayer/PostDbContext.cs
+++ PorraGironaWeb/DataLayer/PostDbContext.cs
@@ -29,8 +29,8 @@ namespace PorraGironaWeb.DataLayer
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;database=porragirona;uid=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.25-mariadb"));
+//#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                optionsBuilder.UseMySql("server=localhost;database=porragirona;uid=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("10.4.25-mariadb"));
             }
         }
 
